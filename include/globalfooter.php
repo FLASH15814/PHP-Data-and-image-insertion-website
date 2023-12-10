e4d01c96cc92ccfb27f940b42b0c5f5cd6c5bf24
FLASH15814 <ranshiv369@gmail.com>
2023-12-09 22:02:52 -0500

    first commit

---
+++ include/globalfooter.php
@@ -0,0 +1,4 @@
+<link rel = "stylesheet" href="css/style.css" type="text/css">
+
+
+<p style="position:absolute;margin-top:20px;margin-left:800px;color:white">@2023 Marvel. All rights reserved.</p>
---
+++ include/globalheader.php
@@ -0,0 +1,41 @@
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" >
+	
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" ></script>
+<style>
+  nav{
+    margin-top:10px;
+    padding:50px 50px 50px 50px ;
+  }
+  li,a{
+    color:black;
+    
+  }
+
+</style>
+
+    <nav class="navbar navbar-expand-sm bg-light" style="position:relative;" >
+
+  <div class="container-fluid" style="background-color:white;">
+    <!-- Links -->
+    <img src="images/marvel.png" style="height:50px;" alt="">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php" style="color:black">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="display-person.php" style="color:black">View</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signin.php" style="color:black">Sign In</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="addimage.php" style="color:black">Add Images</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="viewimage.php" style="color:black">View Images</a>
+      </li>
+    </ul>
+  </div>
+
+</nav>
